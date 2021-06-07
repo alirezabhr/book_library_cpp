070ceca8e00a1977d59afda68f1a5060af1f14e1
Alireza <alirezabahrololoom@gmail.com>
2021-06-07 16:54:00 +0430

    init commit

---
+++ cmake-build-debug/CMakeFiles/books_library.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/books_library.dir/main.cpp.obj"
+  "books_library.exe"
+  "books_library.exe.manifest"
+  "books_library.pdb"
+  "libbooks_library.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/books_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
