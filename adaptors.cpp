6fc02fb352ce152837f36b554bdcb9c29a321472
Alireza <alirezabahrololoom@gmail.com>
2021-06-08 16:39:18 +0430

    adaptors classes added without implementations

--- adaptors.cpp
+++ adaptors.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "adaptors.h"
+
+FixedRecordAdap::FixedRecordAdap(int fixRecSize) : fixRecSize(fixRecSize) {
+
+}
+
+FixedStringAdap::FixedStringAdap(int fixNameSize, int fixLastNameSize) : fixNameSize(fixNameSize),
+                                                                         fixLastNameSize(fixLastNameSize) {
+
+}
+
+FixRecFixStrAdap::FixRecFixStrAdap(int fixRecSize, int fixNameSize, int fixLastNameSize) : FixedRecordAdap(fixRecSize),
+                                                                                           FixedStringAdap(fixNameSize, fixLastNameSize) {
+    data = new char[fixRecSize];
+    name = new char[fixNameSize];
+    lastName = new char[fixLastNameSize];
+}
+
--- adaptors.h
+++ adaptors.h
@@ -1,8 +1,51 @@
-//
-// Created by Alireza on 6/7/2021.
-//
+#include <string>
+#include "entities.h"
 
-#ifndef BOOKS_LIBRARY_ADAPTORS_H
-#define BOOKS_LIBRARY_ADAPTORS_H
+class Adaptor {
+public:
+    int recSize = 0;
+    int nSize = 0;
+    int lnSize = 0;
+    virtual string writeStudent(Student &s) = 0;
+};
 
-#endif //BOOKS_LIBRARY_ADAPTORS_H
+class FixedRecordAdap: public Adaptor {
+    int fixRecSize;
+public:
+    FixedRecordAdap(int fixRecSize);
+};
+
+class DynamicRecordAdap: public Adaptor {
+public:
+};
+
+class FixedStringAdap: public Adaptor {
+    int fixNameSize;
+    int fixLastNameSize;
+public:
+    FixedStringAdap(int fixNameSize, int fixLastNameSize);
+};
+
+class DynamicStringAdap: public Adaptor {
+public:
+};
+
+class FixRecFixStrAdap: public FixedRecordAdap, public FixedStringAdap {
+    char *data;
+    char *name;
+    char *lastName;
+public:
+    FixRecFixStrAdap(int fixRecSize, int fixNameSize, int fixLastNameSize);
+};
+
+class FixRecDynStrAdap: public FixedRecordAdap, public DynamicStringAdap {
+
+};
+
+class DynRecFixStrAdap: public DynamicRecordAdap, public FixedStringAdap {
+
+};
+
+class DynRecDynStrAdap: public DynamicRecordAdap, public DynamicStringAdap {
+
+};
