1b8f911efeb7b2c5e5ac93774163cc0e013c6de7
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-06-21 16:13:31 +0430

    main file changed and some macros added

--- main.cpp
+++ main.cpp
@@ -3,43 +3,67 @@
 
 using namespace std;
 
+#define ADD_STUDENT_OPTION 1
+#define ADD_BOOK_OPTION 2
+#define EXIT_PROGRAM_OPTION 3
+
 int main() {
     Config config;
+    Adaptor *adaptor = nullptr;
 
     try {
         config = getAdaptorOptions();
-    } catch (CONFIG_EXCEPTION ce) {
+    } catch (CONFIG_EXCEPTION &ce) {
         cout << "\aError (in config file): " << ce.getMsg() << endl;
         return 0;
     }
 
-    showMainMenu();
-
     if (config.getRecordMode() == "Fix") {
         if (config.getStringMode() == "Fix") {
             cout << "fix rec fix str" << endl;
-            FixRecFixStrAdap adaptor(config);
-//            int recSize = config.getRecordSize();
-//            int strSize = config.getStudentNameSize() + config.getStudentLastNameSize() + 2;
-//            Student student = getStudent(config.getStudentNameSize(), config.getStudentLastNameSize());
-//            FixRecFixStrAdap adaptor(config, recSize, strSize);
-//            File file(&adaptor);
-//            file.write(student);
-//            Student testStd;
-//            file.read(2, testStd);
-        }
-        if (config.getStringMode() == "Dyn") {
+            adaptor = new FixRecFixStrAdap(config);
+        } else {
             cout << "fix rec dyn str" << endl;
+            adaptor = new FixRecDynStrAdap(config);
         }
-    }
-    if (config.getRecordMode() == "Dyn") {
+    } else {
         if (config.getStringMode() == "Fix") {
             cout << "dyn rec fix str" << endl;
-        }
-        if (config.getStringMode() == "Dyn") {
+            adaptor = new DynRecFixStrAdap(config);
+        } else {
             cout << "dyn rec dyn str" << endl;
+            adaptor = new DynRecDynStrAdap(config);
         }
     }
 
+    while (true) {
+
+        int option = showMainMenu();
+
+        switch (option) {
+            case ADD_STUDENT_OPTION: {
+                cout << "request for adding a student" << endl;
+                Student student = getStudent(config.getStudentNameSize(), config.getStudentLastNameSize());
+                File file(adaptor, &student);
+                file.write();
+//                file.write(student);
+//            Student testStd;
+//            file.read(2, testStd);
+            }
+                break;
+            case ADD_BOOK_OPTION: {
+                cout << "request for adding a book" << endl;
+            }
+                break;
+            case EXIT_PROGRAM_OPTION:
+                cout << "Program Closed" << endl;
+                break;
+            default:
+                cout << "bad request!" << endl;
+        }
+        if (option == EXIT_PROGRAM_OPTION) {
+            break;
+        }
+    }
     return 0;
 }
