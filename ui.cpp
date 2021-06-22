1469d89580dd47dd982438f30facab5c44abcfaa
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-06-22 19:32:19 +0430

    findObjectMenu added in ui files, to find a specific object with specific field

--- ui.cpp
+++ ui.cpp
@@ -5,15 +5,6 @@ using namespace std;
 #define MAX_MENU_OPTIONS 7
 
 
-bool check_number(const string &str) {
-    for (char i : str) {
-        if (isdigit(i) == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int showMainMenu() {    //return int number from [1(add student), 2(all students), ...]
     string inputNum;
     int inputNumInt;
@@ -45,41 +36,6 @@ int showMainMenu() {    //return int number from [1(add student), 2(all students
 
     //system("cls");
     return inputNumInt;
-}
-
-Config getAdaptorOptions() {
-    vector<string> lines;
-    vector<string> tmp;
-    vector<string> commands;
-    string line;
-    Config config;
-
-    ifstream infile;
-    infile.open("config.txt");
-
-    while (getline(infile, line)) {
-        if (line.empty()) {
-            continue;
-        }
-        if (line.at(0) == '#') {
-            continue;
-        }
-        lines.push_back(line);
-    }
-
-    for (auto &it : lines) {
-        tmp = myTokenize(it);
-        commands.push_back(tmp.at(0));
-        commands.push_back(tmp.at(1));
-    }
-
-    for (int i=0; i<commands.size(); i+=2) {
-        string cmd1 = commands.at(i);
-        string cmd2 = commands.at(i+1);
-        config.setFields(cmd1, cmd2);
-    }
-
-    return config;
 }
 
 int getRecordSize() {
@@ -169,111 +125,35 @@ int getLastNameStrSize() {
     return inputNumInt;
 }
 
-vector<string> myTokenize(string line) {
-    string delimiter = " = ";
-    vector<string> v1;
+int findObjectMenu(Object& object) {
+    vector<string> fields = object.getFieldsName();
+    string inputNumStr;
+    int inputNum = 0;
+    bool isValidNum;
 
-    int pos = 0;
-    string token;
-    while ((pos = line.find(delimiter)) != string::npos) {
-        token = line.substr(0, pos);
-        v1.push_back(token);
-        line.erase(0, pos + delimiter.length());
+    cout << "find " << object.getObjectFileName() << " by:" << endl;
+    for (int i = 0; i < fields.size(); ++i) {
+        cout << i+1 << ". " << fields.at(i) << endl;
     }
-    v1.push_back(line);
 
-    return v1;
-}
-
-CONFIG_EXCEPTION::CONFIG_EXCEPTION(const string &msg) : msg(msg) {}
-
-const string &CONFIG_EXCEPTION::getMsg() const {
-    return msg;
-}
-
-Config::Config() {
-    recordSize = 0;
-    studentNameSize = 0;
-    studentLastNameSize = 0;
-}
-
-const string &Config::getRecordMode() const {
-    return recordMode;
-}
-
-const string &Config::getStringMode() const {
-    return stringMode;
-}
-
-int Config::getRecordSize() const {
-    return recordSize;
-}
-
-int Config::getStudentNameSize() const {
-    return studentNameSize;
-}
-
-int Config::getStudentLastNameSize() const {
-    return studentLastNameSize;
-}
-
-void Config::setFields(const std::string& field, const std::string& value) {
-    if (field == "RECORD_MODE") {
-        if (value == "Fix") {
-            this->recordMode = value;
-        } else if (value == "Dyn") {
-            this->recordMode = value;
+    while (true) {
+        getline(cin, inputNumStr);
+        isValidNum = check_number(inputNumStr);
+        if (!isValidNum) {
+            //system("cls");
+            cout << "!! PLEASE ENTER A VALID STUDENT ID !!" << endl;
+            continue;
         } else {
-            throw CONFIG_EXCEPTION("Wrong Record Mode!");
-        }
-    } else if (field == "STRING_MODE") {
-        if (value == "Fix") {
-            this->stringMode = value;
-        } else if (value == "Dyn") {
-            this->stringMode = value;
-        } else {
-            throw CONFIG_EXCEPTION("Wrong String Mode!");
-        }
-    } else if (field == "RECORD_SIZE") {
-        if (this->recordMode == "Dyn") {
-            this->recordSize = -1;
-        } else {
-            try {
-                this->recordSize = stoi(value);
-                if (this->recordSize < 0) {
-                    throw CONFIG_EXCEPTION("Record Size Is Not Valid!\nTry A Positive Number");
-                }
-            } catch (exception &err) {
-                throw CONFIG_EXCEPTION("Record Size Is Not Valid!");
+            inputNum = stoi(inputNumStr);
+            if (inputNum >= 1 && inputNum <= fields.size()) {
+                break;
+            } else {
+                //system("cls");
+                cout << "!! PLEASE ENTER A VALID NUMBER !!" << endl;
+                continue;
             }
         }
-    } else if (field == "STD_NAME_SIZE") {
-        if (this->stringMode == "Dyn") {
-            this->studentNameSize = -1;
-        } else {
-            try {
-                this->studentNameSize = stoi(value);
-                if (this->studentNameSize < 0) {
-                    throw CONFIG_EXCEPTION("Student Name Size Is Not Valid!\nTry A Positive Number");
-                }
-            } catch (exception &err) {
-                throw CONFIG_EXCEPTION("Student Name Size Is Not Valid!");
-            }
-        }
-    } else if (field == "STD_LAST_NAME_SIZE") {
-        if (this->stringMode == "Dyn") {
-            this->studentLastNameSize = -1;
-        } else {
-            try {
-                this->studentLastNameSize = stoi(value);
-                if (this->studentLastNameSize < 0) {
-                    throw CONFIG_EXCEPTION("Student Last Name Size Is Not Valid!\nTry A Positive Number");
-                }
-            } catch (exception &err) {
-                throw CONFIG_EXCEPTION("Student Last Name Size Is Not Valid!");
-            }
-        }
-    } else {
-        cout << "WRONG CONFIGURATION KEY!" << endl;
     }
+
+    return inputNum;
 }
--- ui.h
+++ ui.h
@@ -5,45 +5,9 @@
 #include <string>
 #include <vector>
 #include <fstream>
-//#include ""
+#include "entities.h"
 
-class CONFIG_EXCEPTION;
-class Config;
-
-bool check_number(const std::string &str);
 int showMainMenu();
-Config getAdaptorOptions();
-std::vector<std::string> myTokenize(std::string line);
-//int findObjectMenu(Object& object);
-
-class CONFIG_EXCEPTION {
-    std::string msg;
-public:
-    CONFIG_EXCEPTION(const std::string &msg);
-
-    const std::string &getMsg() const;
-};
-
-class Config {
-    std::string recordMode;
-    std::string stringMode;
-    int recordSize;
-    int studentNameSize;
-    int studentLastNameSize;
-public:
-    Config();
-
-    const std::string &getRecordMode() const;
-
-    const std::string &getStringMode() const;
-
-    int getRecordSize() const;
-
-    int getStudentNameSize() const;
-
-    int getStudentLastNameSize() const;
-
-    void setFields(const std::string& field, const std::string& value);
-};
+int findObjectMenu(Object& object);
 
 #endif //BOOKS_LIBRARY_UI_H
