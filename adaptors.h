7c834ffe0ef3ee166da066b4dd3c3eeb1e33c1b4
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-06-23 22:53:22 +0430

    config class and config_exception class moved to configurations files

--- adaptors.h
+++ adaptors.h
@@ -4,39 +4,12 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include "configurations.h"
 
 using namespace std;
 
 
 class Adaptor;
-class Config;
-
-Config getAdaptorOptions();
-
-class CONFIG_EXCEPTION {
-    string msg;
-public:
-    CONFIG_EXCEPTION(const string &msg);
-
-    const string &getMsg() const;
-};
-
-class Config {
-    string recordMode;
-    string stringMode;
-    int recordSize;
-    int studentNameSize;
-    int studentLastNameSize;
-public:
-    Config();
-    const string &getRecordMode() const;
-    const string &getStringMode() const;
-    int getRecordSize() const;
-    int getStudentNameSize() const;
-    int getStudentLastNameSize() const;
-    void setFields(const string& field, const string& value);
-    static vector<string> tokenize(string line);
-};
 
 class Adaptor {
 protected:
